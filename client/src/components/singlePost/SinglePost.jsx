74cdff76031c9dca1e5119a1625f73629e2c3376
chaitanya40011 <chaitanyaind2022@gmail.com>
2022-11-12 13:55:45 +0530

    Updated Commit

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -11,7 +11,7 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "http://localhost:5000/images/";
+  const PF = process.env.REACT_APP_API_URL + "/images/";
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
